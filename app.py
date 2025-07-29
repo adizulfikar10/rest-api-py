8f7d5b6e9332e05386eea0baecf0291e1871efb7
Adi Zulfikar <adizulf@gmail.com>
2025-07-29 16:16:12 +0700

    feat(): cors localhost

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ from api.db import engine
 import os
 from dotenv import load_dotenv
 from flask_seeder import FlaskSeeder
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 load_dotenv()
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Enable CORS for frontend
+CORS(app, origins=["http://localhost:3000"])
 
 app.register_blueprint(category_bp, url_prefix='/api')
 app.register_blueprint(transaction_bp, url_prefix='/api')
